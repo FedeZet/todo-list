9477f44d063e4ff3f4e420eea7edc377900a2866
FedeZet <fcoelhovazquez@gmail.com>
2024-09-15 19:56:33 -0300

    initial commit
    Create project Todo List API

---
+++ src/test/kotlin/dev/fedezet/todo_list/TodoListApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.fedezet.todo_list
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
